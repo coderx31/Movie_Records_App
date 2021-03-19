fba7eec2ceb0a75fa73fd22aa373b872429de004
coderx31 <shenal.fernando10@gmail.com>
2021-03-19 20:16:15 +0530

    status bar removed from the splash screen

--- app/src/main/java/com/coderx/assignment02/SplashActivity.java
+++ app/src/main/java/com/coderx/assignment02/SplashActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.View;
 
 import java.util.Objects;
 
@@ -15,6 +16,10 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Objects.requireNonNull(getSupportActionBar()).hide(); // hiding the title bar
+        // Hide the status bar
+        View view = getWindow().getDecorView();
+        int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
+        view.setSystemUiVisibility(uiOptions);
         setContentView(R.layout.activity_splash);
 
         /*creating new Handler method to go MainActivity*/
