817a98b63c5f05c5e494b0ad7702afafc9265bfa
coderx31 <shenal.fernando10@gmail.com>
2021-03-29 00:14:41 +0530

    filter implemented and tested

--- app/src/main/java/com/coderx/assignment02/SearchActivity.java
+++ app/src/main/java/com/coderx/assignment02/SearchActivity.java
@@ -1,16 +1,26 @@
 package com.coderx.assignment02;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class SearchActivity extends AppCompatActivity {
+    private static final String TAG = "SearchActivity";
     private EditText user_input;
     private ImageButton imageButton;
     private RecyclerView resultMovieView;
+    private SearchRecViewAdapter adapter;
+    private MoviesData moviesData;
+    private ArrayList<Movie> movies = new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,11 +28,35 @@ public class SearchActivity extends AppCompatActivity {
 
         //calling the initViews method
         initViews();
+
+        movies = moviesData.retrieveData(moviesData.getCursor(moviesData));
+        adapter.setMovies(movies);
+        resultMovieView.setAdapter(adapter);
+        resultMovieView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL,false));
+
+
+
+        imageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //TODO
+                // get the char sequence from user_input
+                // with that call the search filter
+                Log.d(TAG, "onClick: clicked");
+                Toast.makeText(SearchActivity.this, "Clicked!", Toast.LENGTH_SHORT).show();
+                String user_text = user_input.getText().toString().toLowerCase();
+                adapter.getFilter().filter(user_text);
+                Log.d(TAG, "onClick: done!");
+            }
+        });
     }
 
     private void initViews(){
         user_input = findViewById(R.id.user_input);
         imageButton = findViewById(R.id.imageButton);
         resultMovieView = findViewById(R.id.resultMovieView);
+
+        moviesData = new MoviesData(this);
+        adapter = new SearchRecViewAdapter(this);
     }
 }
--- app/src/main/java/com/coderx/assignment02/SearchRecViewAdapter.java
+++ app/src/main/java/com/coderx/assignment02/SearchRecViewAdapter.java
@@ -1,7 +1,12 @@
 package com.coderx.assignment02;
 
 import android.content.Context;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Filter;
+import android.widget.Filterable;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -9,11 +14,85 @@ import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
+import java.util.Collection;
 
-public class SearchRecViewAdapter {
+public class SearchRecViewAdapter extends RecyclerView.Adapter<SearchRecViewAdapter.ViewHolder> implements Filterable {
     private static final String TAG = "SearchViewAdapter";
     private ArrayList<Movie> movies = new ArrayList<>();
     private Context mContext;
+    private ArrayList<Movie> allMovies;
+
+    public SearchRecViewAdapter(Context context){
+        this.mContext = context;
+        this.allMovies = new ArrayList<>(movies);
+    }
+
+
+    public void setMovies(ArrayList<Movie> movies) {
+        this.movies = movies;
+        notifyDataSetChanged();
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.search_movie_list,parent,false);
+        ViewHolder holder = new ViewHolder(view);
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        Log.d(TAG, "onBindViewHolder: Called!");
+        holder.txtTitle.setText(movies.get(position).getTitle()); // setting the movie title to text view
+    }
+
+    @Override
+    public int getItemCount() {
+        return movies.size();
+    }
+
+    @Override
+    public Filter getFilter() {
+        return filter;
+    }
+
+    Filter filter = new Filter() {
+        // run on background thread
+        @Override
+        protected FilterResults performFiltering(CharSequence charSequence) {
+            ArrayList<Movie> filteredList = new ArrayList<>();
+            if (charSequence.toString().isEmpty()){
+                Log.d(TAG, "performFiltering: on start");
+                // nothing to do
+                // nothing to display
+            }else{
+                // display filtered items
+                //TODO
+                //needs to implement filter for director and actor names
+                for (Movie movie : movies){
+                    Log.d(TAG, "performFiltering: after button clicked");
+                    if (movie.getTitle().toLowerCase().contains(charSequence.toString().toLowerCase())){
+                        filteredList.add(movie);
+                    }
+                }
+            }
+
+            FilterResults filterResults = new FilterResults();
+            filterResults.values = filteredList;
+
+            return filterResults;
+        }
+
+        // runs on a ui thread
+        @Override
+        protected void publishResults(CharSequence charSequence, FilterResults filterResults) {
+            movies.clear();
+            movies.addAll((Collection<? extends Movie>) filterResults.values);
+            notifyDataSetChanged();
+        }
+    };
+
     public class ViewHolder extends RecyclerView.ViewHolder{
         private CardView parent;
         private TextView txtTitle;
