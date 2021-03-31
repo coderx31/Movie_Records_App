db148cbd84c0bd903d0f23c8e3596b4264be4caa
coderx31 <shenal.fernando10@gmail.com>
2021-03-31 08:32:58 +0530

    rating list created

--- app/src/main/java/com/coderx/assignment02/RatingActivity.java
+++ app/src/main/java/com/coderx/assignment02/RatingActivity.java
@@ -1,14 +1,58 @@
 package com.coderx.assignment02;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class RatingActivity extends AppCompatActivity {
-
+    private RecyclerView ratingRecView;
+    private Button btnFind;
+    private RatingRecViewAdapter adapter;
+    private MoviesData moviesData;
+    private ArrayList<Movie> movies = new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rating);
+
+        //calling the initViews method
+        initViews();
+
+        movies = moviesData.retrieveData(moviesData.getCursor(moviesData));
+        ratingRecView.setAdapter(adapter);
+        adapter.setMovies(movies);
+        ratingRecView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
+
+        btnFind.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //TODO
+                String selectedMovie = RatingRecViewAdapter.movieTitle;
+                if (selectedMovie == null){
+                    Toast.makeText(RatingActivity.this, "Please Select a Movie", Toast.LENGTH_SHORT).show();
+                }else{
+                    Intent intent = new Intent(RatingActivity.this, SelectedRatingViewActivity.class);
+                    intent.putExtra("movie",selectedMovie);
+                    startActivity(intent);
+                }
+
+            }
+        });
+    }
+
+    private void initViews(){
+        ratingRecView = findViewById(R.id.ratingRecView);
+        btnFind = findViewById(R.id.btnFind);
+
+        adapter = new RatingRecViewAdapter(this);
+        moviesData = new MoviesData(this);
     }
 }
---
+++ app/src/main/java/com/coderx/assignment02/RatingRecViewAdapter.java
@@ -0,0 +1,98 @@
+package com.coderx.assignment02;
+
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.RadioButton;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class RatingRecViewAdapter extends RecyclerView.Adapter<RatingRecViewAdapter.ViewHolder> {
+    private static final String TAG = "RatingRecViewAdapter";
+    public static String movieTitle;
+    private ArrayList<Movie> movies = new ArrayList<>();
+    private Context mContext;
+    private int lastCheckedPosition = -1;
+
+    public RatingRecViewAdapter(Context context){
+        this.mContext = context;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.rating_list,parent,false);
+        ViewHolder holder = new ViewHolder(view);
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull final ViewHolder holder, final int position) {
+        Log.d(TAG, "onBindViewHolder: called!");
+        holder.txtTitle.setText(movies.get(position).getTitle()); // setting the title of each movies
+        holder.titleSelect.setChecked(position == lastCheckedPosition);
+
+
+
+        holder.titleSelect.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                movieTitle = movies.get(position).getTitle();
+                Toast.makeText(mContext, movies.get(position).getTitle()+ " Selected ", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        holder.parent.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //TODO
+                // implementation
+                Intent intent = new Intent(mContext,SelectedRatingViewActivity.class);
+                intent.putExtra("movie",movies.get(position).getTitle());
+                mContext.startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return movies.size();
+    }
+
+    public void setMovies(ArrayList<Movie> movies) {
+        this.movies = movies;
+        notifyDataSetChanged();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+        private CardView parent;
+        private TextView txtTitle;
+        private RadioButton titleSelect;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            parent = itemView.findViewById(R.id.parent);
+            txtTitle = itemView.findViewById(R.id.txtTitle);
+            titleSelect = itemView.findViewById(R.id.titleSelect);
+
+            titleSelect.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    lastCheckedPosition = getAdapterPosition();
+                    notifyDataSetChanged();
+                }
+            });
+        }
+    }
+}
